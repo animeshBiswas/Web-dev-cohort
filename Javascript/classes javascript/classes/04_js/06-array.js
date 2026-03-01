123c0c8fbf2c5aa92bb8a6f787ce88b8b05bc88b
Animesh Biswas <109897067+animeshBiswas@users.noreply.github.com>
2026-03-01 09:57:41 +0000

    Arrays

---
+++ Javascript/classes javascript/classes/04_js/06-array.js
@@ -0,0 +1,14 @@
+const carriage1 = ["Animesh","Govind","Biswas"]
+const emptyCarriage = []
+
+const threeEmptySeats = Array(3)
+console.log(threeEmptySeats); // [ <3 empty items> ]
+console.log(threeEmptySeats.length); // 3
+
+const passenger = Array("Animesh","Govind","Biswas")
+
+const singlePassenger = Array.of(3);
+const anotherPassenger = Array.of("Animesh");
+console.log(singlePassenger); // [ 3 ]
+console.log(anotherPassenger); // [ 'Animesh' ]
+// Research about Array() and Array.of() and also learn about Array.of()
