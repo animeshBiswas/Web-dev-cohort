d213a730b3295c264bcc93a33c8920d8fcd81c70
Animesh Biswas <109897067+animeshBiswas@users.noreply.github.com>
2026-03-04 11:21:35 +0000

    objects

--- Javascript/classes javascript/classes/04_js/08-objects.js
+++ Javascript/classes javascript/classes/04_js/08-objects.js
@@ -10,3 +10,19 @@ const hero = {
 // There are two types to access values inside a objects :-
 // 1. hero. or hero[dot] (just for understanding) to access any properties, when you know 100% that this is the only values is not coming from a API or dynamically then use hero[dot] when you hardcode the values
 // 2. hero[] :- its coming from response, accumulator or you're processing from the data then use hero[]
+
+hero.weapon = "Fire"
+
+delete hero.level
+
+const ranger = {
+    name: "Lakshya the swift",
+    agility: 80,
+    stealth: undefined
+};
+
+console.log("name" in ranger);
+console.log("stealth" in ranger);
+console.log("toString" in ranger); // o/p is true and the reasons behind is the prototype chaining and when you write the objects in the above is also comes from the further objects also know nested objects and "ranger" inherits other properties too and thats why "toString" key is true
+// to check without any issues then use the .hasOwnProprty() to get reliable answers
+console.log(ranger.hasOwnProperty("toString"));
