cb695c80ffcaeaa0de82a0ed11e0814adc5d5b6b
Animesh Biswas <109897067+animeshBiswas@users.noreply.github.com>
2026-03-23 18:57:42 +0000

    update symbol

--- Javascript/classes javascript/classes/04_js/16-Symbol.js
+++ Javascript/classes javascript/classes/04_js/16-Symbol.js
@@ -44,7 +44,32 @@ console.log(Object.keys(citizenRecord));
 console.log(Object.getOwnPropertySymbols(citizenRecord));
 // [ Symbol(biometricHash), Symbol(bloodGroup) ]
 
+const rtiQueryBook = {
+    queries: ['Infra budget', 'Ration Card', 'Education budget', 'startup laws'],
+}
 
+for(const query of rtiQueryBook){
+    console.log(`Filing RTI: ${query}`);
+    [Symbol.iterator](){
+        let index = 0;
+        const queries = this.queries
+        return {
+            next() {
+                if (index < queries.length) {
+                    return `Hello`;
+                }
+                return 'nope'
+            }
+        } 
+    } 
+    
+}
+/*
+O/P :-
+TypeError: rtiQueryBook is not iterable
+you cant do loop directly due to arrays inside a object And cant do loops in objects
+to make it iterable you have make some twirk in JS
+*/
 /* Use-case 
 
 
