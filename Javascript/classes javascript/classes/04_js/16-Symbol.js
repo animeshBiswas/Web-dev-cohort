bc0d32ac8c9bfbae9db04c5d13d8230def24b452
Animesh Biswas <109897067+animeshBiswas@users.noreply.github.com>
2026-03-23 18:27:04 +0000

    Symbol

--- Javascript/classes javascript/classes/04_js/16-Symbol.js
+++ Javascript/classes javascript/classes/04_js/16-Symbol.js
@@ -4,6 +4,9 @@ everyone has a aadhar card but the number is unique
 */ 
 
 const aadhaar_of_animesh = Symbol("aadhaar")
+// Symbol("aadhaar"); "aadhaar" is the lable name of symbol
+// kya label gurantte hai ki symbol name same honge? No
+// Symbol only guranttee that you'll get unique values but they wont tells what the unique values is
 const aadhaar_of_piyush = Symbol("aadhaar")
 
 console.log(typeof aadhaar_of_animesh);
@@ -21,19 +24,30 @@ const nonIndian = Symbol();
 console.log(nonIndian.description);
 // undefined => because you give label 
 
-
+// Using symbols as a keys
 const biometricHash = Symbol("biometricHash")
 const bloodGroup = Symbol("bloodGroup")
 
 const citizenRecord = {
     name: "Animesh Biswas",
     age: 24,
-    [biometricHash]: "ajakjdiksjidjhsidj",
+    // you can define key [] this way
+    // you cant able to loop due to non visibility
+    [biometricHash]: "ajakjdiksjidjhsidj", 
     [bloodGroup]: "O+"
 }
 
 console.log(Object.keys(citizenRecord));
-// [ 'name', 'age' ]
+// [ 'name', 'age' ] you see different behavioue and key you'll get is name and age; and making a symbol and passing a key so you wont able to see the keys, this not visible properties
 
+// to find out non-visible properties
 console.log(Object.getOwnPropertySymbols(citizenRecord));
 // [ Symbol(biometricHash), Symbol(bloodGroup) ]
+
+
+/* Use-case 
+
+
+
+
+*/
